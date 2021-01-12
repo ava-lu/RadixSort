6e048d3f8db2fddb7d3a8db67774bf4217e97e08
ava-lu <72944599+ava-lu@users.noreply.github.com>
2021-01-12 15:44:14 -0500

    Delete MyTester.java

--- MyTester.java
+++
@@ -1,8 +0,0 @@
-public class MyTester {
-  public static void main(String[] args) {
-    //nth
-    // System.out.println(Radix.nth(123,0));
-    // System.out.println(Radix.nth(38043930,0));
-
-  }
-}
