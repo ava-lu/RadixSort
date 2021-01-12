bd1b6de9b30ed081fc7c03e46d217a5e77664811
ava-lu <alu21@stuy.edu>
2021-01-12 17:38:40 -0500

    finished and tested length()

--- Radix.java
+++ Radix.java
@@ -3,4 +3,7 @@ public class Radix {
     int temp = Math.abs(n / (int)Math.pow(10, col));
     return (temp % 10);
   }
+  public static int length(int n) {
+    return String.valueOf(n).length();
+  }
 }
