e3886655dd629d5f9135de76e8953844de28b5d7
ava-lu <alu21@stuy.edu>
2021-01-12 19:25:18 -0500

    finished and tested length

--- MyTester.java
+++ MyTester.java
@@ -4,5 +4,9 @@ public class MyTester {
     // System.out.println(Radix.nth(123,0));
     // System.out.println(Radix.nth(38043930,0));
 
+    //length
+    // System.out.println(Radix.length(1111));
+    // System.out.println(Radix.length(23));
+    // System.out.println(Radix.length(383));
   }
 }
